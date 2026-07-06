8999806bab982af318d88bad412f69ea9f5748e1
hanakin <midaym@gmail.com>
2026-07-05 23:08:19 -1000

    fix custom toggle

--- registry/components/ui/cva/toggle.ts
+++ registry/components/ui/cva/toggle.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 const toggle = cva(
-	"group/toggle inline-flex items-center justify-center gap-1 rounded-md text-sm font-medium whitespace-nowrap transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 aria-pressed:bg-muted dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 hover:bg-interactive/50 hover:text-primary focus-visible:bg-primary focus-visible:text-current",
+	"group/toggle inline-flex items-center justify-center gap-1 rounded-md text-sm font-medium whitespace-nowrap transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 hover:bg-interactive/50 hover:text-primary focus-visible:bg-primary focus-visible:text-current aria-pressed:bg-primary aria-pressed:text-current",
 	{
 		variants: {
 			variant: {
